368e3e66f8c432bfd95153c8143987997075ed5b
rixx <git@foyfoy.de>
2014-02-02 13:04:00 +0100

    implemented Plenum.plenum and Plenum.plenumlink

--- plugins/Plenum/plugin.py
+++ plugins/Plenum/plugin.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 ###
 # Copyright (c) 2014, rixx
 # All rights reserved.
@@ -35,11 +36,49 @@ import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
 
+from urllib2 import urlopen
+from datetime import date, timedelta
+
 class Plenum(callbacks.Plugin):
     """Add the help for "@plugin help Plenum" here
     This should describe *how* to use this plugin."""
-    pass
 
+    def __init__(self, irc):
+        self.__parent = super(Plenum, self)
+        self.__parent.__init__(irc)
+
+    def plenum(self, irc, msg, args):
+        """ takes no arguments
+
+        prints date and URL of the next shackspace plenum"""
+        datum = self.get_plenum_date()
+        delta = (datum - date.today()).days
+
+        if delta == 0:
+            reply_string = "Heute ist Plenum!"
+        else:
+            reply_string = "Das nächste Plenum ist in " + str(delta) + " Tagen, "
+            reply_string += "am " + datum.strftime("%d.%m.")
+
+        irc.reply(reply_string, prefixNick=False)
+
+    plenum = wrap(plenum)
+
+    def plenumlink(self, irc, msg, args):
+        """ takes no arguments
+
+        prints URL to the next plenum protocol"""
+
+        irc.reply(urlopen('http://shackspace.de/nextplenum/http300/current').geturl(),\
+                  prefixNick=False)
+
+    plenumlink = wrap(plenumlink)
+
+    def get_plenum_date(self):
+        raw = urlopen("http://shackspace.de/nextplenum/text/iso").read()[:10]
+        plenum_date = date(int(raw[:4]), int(raw[6:7]), int(raw[9:10]))
+
+        return plenum_date
 
 Class = Plenum
 
